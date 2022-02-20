e08a8329e3b9148c5dce85e59518d94bb93e00c7
Martin van Zijl <martin.vanzijl@gmail.com>
2022-02-21 12:04:18 +1300

    Add unit test framework.

--- app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
@@ -1,14 +1,18 @@
 package com.example.myfirstapp;
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +20,15 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myfirstapp", appContext.getPackageName());
+    public void pingButtonLabelCorrect() {
+        onView(withId(R.id.buttonPing)).check(matches(withText(R.string.ping_button_label)));
     }
 }
