6664a478089974573440aa3f7f86143e1bb0b2e0
Martin van Zijl <martin.vanzijl@gmail.com>
2022-07-27 08:04:58 +1200

    Add unit test (WIP).

--- app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
@@ -1,18 +1,20 @@
 package com.example.myfirstapp;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -30,5 +32,19 @@ public class ExampleInstrumentedTest {
     @Test
     public void pingButtonLabelCorrect() {
         onView(withId(R.id.buttonPing)).check(matches(withText(R.string.ping_button_label)));
+    }
+
+    @Test
+    public void startScheduledPingLabelCorrect() {
+        // Open the menu.
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
+
+        // DOES NOT WORK! The action is not found.
+
+        // Click the menu item to go to the "Scheduled Pings" screen.
+        onView((withId(R.id.action_scheduled_ping))).perform(click());
+
+        // Check the original button label.
+        onView(withId(R.id.buttonScheduleSave)).check(matches(withText(R.string.button_schedule_start_label)));
     }
 }
