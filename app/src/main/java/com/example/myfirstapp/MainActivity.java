f3bd71f07210b54c64f04b76fd8ed99d8cc0228c
Martin van Zijl <martin.vanzijl@gmail.com>
2022-09-09 10:56:17 +1200

    Address warnings from Android Studio (WIP).
    Fixes #14.

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -848,7 +848,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
