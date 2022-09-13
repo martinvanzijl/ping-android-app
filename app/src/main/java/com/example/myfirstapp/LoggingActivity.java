6ce874bc6d930b3ef7c5d6ddb93e439b6dce0864
Martin van Zijl <martin.vanzijl@gmail.com>
2022-09-14 11:36:19 +1200

    Remove unused import.

--- app/src/main/java/com/example/myfirstapp/LoggingActivity.java
+++ app/src/main/java/com/example/myfirstapp/LoggingActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.IOException;
